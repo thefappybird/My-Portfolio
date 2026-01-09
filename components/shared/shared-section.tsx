c077f119560f5f795263999fd2aa954726bdc6cb
thefappybird <alex.banaag1@gmail.com>
2026-01-09 19:08:12 +0400

    small UI fix

--- components/shared/shared-section.tsx
+++ components/shared/shared-section.tsx
@@ -14,7 +14,7 @@ function SharedSection({
   return (
     <section
       id={id}
-      className={`px-4 py-18 ${bg} flex items-center justify-center mx-auto`}
+      className={`px-4 py-18 ${bg} flex items-center justify-center mx-auto w-full`}
     >
       <div className="flex flex-col max-w-7xl gap-6 justify-center mx-auto">
         {title && <h2 className="text-4xl">{title}</h2>}
